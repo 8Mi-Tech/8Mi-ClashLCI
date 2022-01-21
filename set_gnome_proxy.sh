3dd908d0a7afde80a038134c7a095a3dcf8ff742
8Mi_Yile <admin@8mi.tech>
2022-01-22 01:16:38 +0800

    减少重复文件

--- set_gnome_proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#8Mi-ClashController <Powered By Clash>
-gsettings set org.gnome.system.proxy mode 'manual'
-gsettings set org.gnome.system.proxy.http port 7890
-gsettings set org.gnome.system.proxy.http host '127.0.0.1'
-gsettings set org.gnome.system.proxy.socks port 7891
-gsettings set org.gnome.system.proxy.socks host '127.0.0.1'
-gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8', '::1']"
