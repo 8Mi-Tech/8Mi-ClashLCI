ccd0a525e017108035f8dad342eacca31fa60422
8Mi_Yile <admin@8mi.tech>
2022-01-22 01:15:17 +0800

    大幅度优化写法,直接按照类服务控制事件来编写

--- reloadClash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#8Mi-ClashController <Powered By Clash>
-export http_proxy=
-export https_proxy=
-export ftp_proxy=
-wget --show-progress --progress=bar:force -O config.yaml "$(grep "sub_link=" clash_settings.config|sed 's/sub_link=//g')" 2>&1| tail -f -n +8
-tmux send-keys -t clash:core C-c './clash -d ./' Enter
