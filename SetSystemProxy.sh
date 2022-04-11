291e0cf07f3d7a0493b2e4313141be37247ca2a6
8Mi_Yile <admin@8mi.tech>
2022-04-11 12:34:14 +0800

    典型的白痴修复方法（草）

--- SetSystemProxy.sh
+++ SetSystemProxy.sh
@@ -2,16 +2,18 @@
 #SetSystemProxy in Root Perm
 if [ "$1" = "default" ] | [ "$2" = "true" ]; then
 echo '#!/bin/bash
-http_proxy=http://127.0.0.1:7890
-https_proxy=http://127.0.0.1:7890
-SOCKS_SERVER=socks5://127.0.0.1:7890'>/etc/profile.d/8Mi-ClashLCI_SystemProxy.sh
+http_proxy=http://127.0.0.1:7899
+https_proxy=http://127.0.0.1:7899
+SOCKS_SERVER=socks5://127.0.0.1:7899'>/etc/profile.d/8Mi-ClashLCI_SystemProxy.sh
 else if [ "$1" == default ]; then
 rm /etc/profile.d/8Mi-ClashLCI_SystemProxy.sh
 fi
+fi
 
 if [ "$1" = "apt" ] | [ "$2" = "true" ]; then
-echo 'Acquire::http::Proxy "http://127.0.0.1:7890";
-Acquire::https::Proxy "http://127.0.0.1:7890";'>/etc/apt/apt.conf.d/8Mi-ClashLCI_ProxyConfig
+echo 'Acquire::http::Proxy "http://127.0.0.1:7899";
+Acquire::https::Proxy "http://127.0.0.1:7899";'>/etc/apt/apt.conf.d/8Mi-ClashLCI_ProxyConfig
 else if [ "$1" = "apt" ]; then
 rm /etc/apt/apt.conf.d/8Mi-ClashLCI_ProxyConfig
+fi
 fi
